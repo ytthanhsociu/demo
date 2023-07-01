3f3ccd3702f9168d5c7ff3a56748c6c105843694
ThanhSociu <ytthanhsociu@gmail.com>
2023-07-01 17:00:59 +0700

    addd

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -16,6 +16,7 @@ public class Demo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("");
+        System.out.println("");
     }
     
 }
